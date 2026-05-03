03dac15aa5bf13f853ce7b206af5ac336742f7bc
KillerShady <stofanik369@gmail.com>
2026-05-03 11:16:21 +0200

    added css for view-mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import TaskComponent from "./features/mainTask/TaskComponent.tsx";
 import {Container} from "react-bootstrap";
 import ImportExportComponent from "./features/import/ImportExportComponent.tsx";
 
-export default function App() {
+export default function App({ viewOnly }: { viewOnly: boolean }) {
     return (
-        <Container fluid>
-            <ImportExportComponent />
-            <LanguageComponent />
-            <TaskComponent />
-        </Container>
+        <div className="eqv-trans-checker">
+            <Container fluid className={viewOnly ? "view-mode" : ""}>
+                <ImportExportComponent />
+                <LanguageComponent />
+                <TaskComponent />
+            </Container>
+        </div>
     )
 }
