f7a5b12ce5add2ef6c7e2d4d9d029e1f5b4aebf4
KillerShady <stofanik369@gmail.com>
2026-06-05 14:53:06 +0200

    fixed compatibility with Logic Workbook

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom";
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "katex/dist/katex.min.css";
+import 'katex/dist/katex.min.css'
 import {AppComponent, prepare} from "./AppComponent.tsx";
-import type {LogicContext} from "./LogicContext.ts";
+//import type {LogicContext} from "./LogicContext.ts";
 
-const context: LogicContext = {
+/*const context: LogicContext = {
     constants: ["kitty"],
     predicates: [{name: "cat", arity:1},{name: "loves", arity:2}],
     functions: [],
@@ -14,12 +14,15 @@ const context: LogicContext = {
     formulas: [{name:"F1", formula:"\\E x cat(x)"}],
     axioms: [{name:"A1", formula:"\\A z cat(z)"}],
     theorems: [],
-}
+}*/
 
 const instance = prepare(null).instance;
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
     <StrictMode>
         <AppComponent instance={instance} isEdited={false} onStateChange={() => {}} context={undefined}/>
     </StrictMode>,
-)
+    document.getElementById("root"),
+);
+
+export default {AppComponent, prepare}
